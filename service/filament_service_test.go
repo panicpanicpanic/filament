89ba931bc84a58c05a012ea1206b2c3323270f71
Angel Martinez <a.angel.martinez@gmail.com>
2018-11-13 23:44:37 -0500

    Add tests for Post service method

--- service/filament_service_test.go
+++ service/filament_service_test.go
@@ -182,7 +182,7 @@ func TestServicePut(t *testing.T) {
 
 		_, err = service.Put(&client, payload)
 		if err == nil {
-			t.Errorf("it should have thrown an error for not supplying an AccessToken, got %d", err)
+			t.Errorf("it should have thrown an error for not supplying an Endpoint, got %d", err)
 		}
 	})
 
@@ -246,6 +246,145 @@ func TestServicePut(t *testing.T) {
 		client.Endpoint = server.URL
 
 		body, err := service.Put(&client, payload)
+		if len(body) == 0 {
+			t.Errorf("it should have returned 1 empty device, got %d", err)
+		}
+		if reflect.TypeOf(body).String() != "[]uint8" {
+			t.Errorf("it should have returned a []byte, got %d", reflect.TypeOf(body))
+		}
+	})
+}
+
+func TestServicePost(t *testing.T) {
+	var client lifx.Client
+	var err error
+
+	t.Run("when AccessToken is missing from lifx.Client", func(t *testing.T) {
+		response := []byte(`
+			{
+				"results": [
+					{
+						"id": "d073d52260ef",
+						"status": "ok",
+						"label": "Main"
+					}
+				]
+			}
+		`)
+
+		payload := []byte(`
+			{
+				"power":"on"
+			}
+		`)
+
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusMultiStatus)
+			w.Write(response)
+		}))
+
+		client.Endpoint = server.URL
+
+		_, err = service.Post(&client, payload)
+		if err == nil {
+			t.Errorf("it should have thrown an error for not supplying an AccessToken, got %d", err)
+		}
+	})
+
+	t.Run("when Endpoint is missing from lifx.Client", func(t *testing.T) {
+		response := []byte(`
+			{
+				"results": [
+					{
+						"id": "d073d52260ef",
+						"status": "ok",
+						"label": "Main"
+					}
+				]
+			}
+		`)
+
+		payload := []byte(`
+			{
+				"power":"on"
+			}
+		`)
+
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusMultiStatus)
+			w.Write(response)
+		}))
+
+		client.AccessToken = server.URL
+		client.Endpoint = ""
+
+		_, err = service.Post(&client, payload)
+		if err == nil {
+			t.Errorf("it should have thrown an error for not supplying an Endpoint, got %d", err)
+		}
+	})
+
+	t.Run("when the LIFX API returns a non-207 HTTP status code for POST request", func(t *testing.T) {
+		response := []byte(`
+			{
+				"results": [
+					{
+						"id": "d073d52260ef",
+						"status": "ok",
+						"label": "Main"
+					}
+				]
+			}
+		`)
+
+		payload := []byte(`
+			{
+				"power":"on"
+			}
+		`)
+
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(response)
+		}))
+
+		client.AccessToken = "someRandomToken"
+		client.Endpoint = server.URL
+
+		_, err = service.Post(&client, payload)
+		if err == nil {
+			t.Errorf("it should have thrown an error for returning a 500 HTTP status, got %d", err)
+		}
+	})
+
+	t.Run("when the expected response is not []byte", func(t *testing.T) {
+		response := []byte(`
+			{
+				"results": [
+					{
+						"id": "d073d52260ef",
+						"status": "ok",
+						"label": "Main"
+					}
+				]
+			}
+		`)
+
+		payload := []byte(`
+			{
+				"power":"on"
+			}
+		`)
+
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write(response)
+		}))
+
+		client.AccessToken = "someRandomToken"
+		client.Endpoint = server.URL
+
+		body, err := service.Post(&client, payload)
 		if len(body) == 0 {
 			t.Errorf("it should have returned 1 empty device, got %d", err)
 		}
